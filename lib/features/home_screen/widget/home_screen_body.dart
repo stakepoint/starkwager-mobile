8700d2585834861230fbe285a9f77e7bd55e0b03
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2025-02-21 13:58:29 +0100

    lint

--- lib/features/home_screen/widget/home_screen_body.dart
+++ lib/features/home_screen/widget/home_screen_body.dart
@@ -63,7 +63,7 @@ class _HomeScreenBodyContentState
           boxShadow: isSelected
               ? [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
+                    color: Colors.black.withValues(alpha: 0.1),
                     blurRadius: 4,
                     offset: const Offset(0, 2),
                   )
@@ -145,7 +145,6 @@ class _HomeScreenBodyContentState
   @override
   Widget build(BuildContext context) {
     final bool isMobile = context.isMobile;
-    final isLandscape = context.isLandscape;
 
     return CustomScrollView(
       physics: const AlwaysScrollableScrollPhysics(),
@@ -196,54 +195,6 @@ class _HomeScreenBodyContentState
           ),
         ),
       ],
-    );
-  }
-
-  Widget _mobileNoWager(BuildContext context) {
-    return Container(
-      height: 81,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(24),
-        color: context.containerColor,
-      ),
-      child: Row(
-        spacing: 16,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          horizontalSpace(16),
-          SvgPicture.asset(AppIcons.noWagerIcon),
-          Text(
-            'noWagersCreatedYet'.tr(),
-            style: AppTheme.of(context).bodyLarge16.copyWith(
-                  color: context.textHintColor,
-                ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _tabletNoWager(BuildContext context) {
-    return Container(
-      height: 175,
-      width: 696,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(24),
-        color: context.containerColor,
-      ),
-      child: Column(
-        spacing: 24,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(AppIcons.noWagerIcon, width: 88, height: 88),
-          Text(
-            'noWagersCreatedYet'.tr(),
-            style: AppTheme.of(context).textMediumNormal.copyWith(
-                  color: context.textHintColor,
-                ),
-          ),
-        ],
-      ),
     );
   }
 }
