4b0c6badbbe0bb7d6b9c9284155ff81ec3582b1c
kedwithGod <Komolafeezekiel123@gmail.com>
2025-02-26 20:41:00 +0100

    adding onTap for the account setting container

--- lib/features/profile_setting/widget/profile_setting_body.dart
+++ lib/features/profile_setting/widget/profile_setting_body.dart
@@ -5,72 +5,81 @@ class ProfileSettingBody extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      children: [
-        Container(
-          height: !context.isMobile ? 81 : null,
-          padding: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: context.containerColor,
-          ),
-          child: Row(
-            spacing: 16,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                  padding: EdgeInsets.all(8),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      // shape: BoxShape.rectangle,
-                      color: context.textBoxTextColor),
-                  child: SvgPicture.asset(
-                    AppIcons.profileIcon2,
-                    height: 24,
-                    width: 24,
-                  )),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'accountSettings'.tr(),
-                    style: AppTheme.of(context).bodyLarge16.copyWith(
-                        color: context.primaryTextColor,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    context.isMobile
-                        ? 'changeYourProfilepictureUsername...'.tr()
-                        : 'changeYourProfilepictureUsername...'
-                            .tr()
-                            .replaceAll('\n', ''),
-                    style: AppTheme.of(context).bodySmall12.copyWith(
-                        color: context.subTitleTextColor,
-                        fontWeight: FontWeight.w400),
-                  )
-                ],
-              ),
-              Spacer(),
-              GestureDetector(
-                onTap: () {
-                  if (context.isMobile) {
-                    GoRouter.of(context).go(Routes.accountSettings);
-                  } else {
-                    GoRouter.of(context).go(Routes.accountSettingsTablet);
-                  }
-                },
-                child: SvgPicture.asset(
-                  AppIcons.nextArrow,
-                  height: 16,
-                  width: 16,
+    return GestureDetector(
+      onTap: () {
+        if (context.isMobile) {
+          GoRouter.of(context).go(Routes.accountSettings);
+        } else {
+          GoRouter.of(context).go(Routes.accountSettingsTablet);
+        }
+      },
+      child: Column(
+        children: [
+          Container(
+            height: !context.isMobile ? 81 : null,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: context.containerColor,
+            ),
+            child: Row(
+              spacing: 16,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                    padding: EdgeInsets.all(8),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        // shape: BoxShape.rectangle,
+                        color: context.textBoxTextColor),
+                    child: SvgPicture.asset(
+                      AppIcons.profileIcon2,
+                      height: 24,
+                      width: 24,
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'accountSettings'.tr(),
+                      style: AppTheme.of(context).bodyLarge16.copyWith(
+                          color: context.primaryTextColor,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Text(
+                      context.isMobile
+                          ? 'changeYourProfilepictureUsername...'.tr()
+                          : 'changeYourProfilepictureUsername...'
+                              .tr()
+                              .replaceAll('\n', ''),
+                      style: AppTheme.of(context).bodySmall12.copyWith(
+                          color: context.subTitleTextColor,
+                          fontWeight: FontWeight.w400),
+                    )
+                  ],
                 ),
-              ),
-            ],
-          ),
-        )
-      ],
+                Spacer(),
+                GestureDetector(
+                  onTap: () {
+                    if (context.isMobile) {
+                      GoRouter.of(context).go(Routes.accountSettings);
+                    } else {
+                      GoRouter.of(context).go(Routes.accountSettingsTablet);
+                    }
+                  },
+                  child: SvgPicture.asset(
+                    AppIcons.nextArrow,
+                    height: 16,
+                    width: 16,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
