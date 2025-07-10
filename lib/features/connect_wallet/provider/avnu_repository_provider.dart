2c4ba574844f3cf66053ff8c8e0a99771775d114
Emrys <Emrys_02@outlook.com>
2025-07-10 11:08:36 +0100

    refactor(avnu): Remove redundant comment in Avnu remote data source provider
    Signed-off-by: Emrys <Emrys_02@outlook.com>

--- lib/features/connect_wallet/provider/avnu_repository_provider.dart
+++ lib/features/connect_wallet/provider/avnu_repository_provider.dart
@@ -5,7 +5,6 @@ import 'package:starkwager/data/datasources/avnu_remote_datasource.dart';
 import 'package:starkwager/data/repositories/avnu_repository_impl.dart';
 import 'package:starkwager/domain/repositories/avnu_repository.dart';
 
-// You may already have a provider for AvnuRemoteDataSource elsewhere. If so, import and use it here.
 final avnuRemoteDataSourceProvider = Provider<AvnuRemoteDataSource>((ref) {
   return AvnuRemoteDataSourceImpl(ApiClient(Dio(BaseOptions(baseUrl: "https://sepolia.api.avnu.fi/"))));
 });
